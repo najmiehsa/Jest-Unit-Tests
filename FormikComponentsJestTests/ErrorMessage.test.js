25aef503010121ec774512e8db02216233b3e8ee
Najmieh Sadat <98463228+najmiehsa@users.noreply.github.com>
2022-07-16 22:35:12 +0430

    Update ErrorMessage.test.js

--- FormikComponentsJestTests/ErrorMessage.test.js
+++ FormikComponentsJestTests/ErrorMessage.test.js
@@ -4,3 +4,9 @@ import {render, screen, waitFor} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 import {ErrorMessage} from './ErrorMessage.js'
+
+test("rendering and Showing the error message component", async () => {
+    render(<SignUp />)
+
+
+});
