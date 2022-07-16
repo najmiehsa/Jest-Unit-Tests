d9e6e8183c2b41aa8cf9a8b8ab4b8b934b1ecf18
Najmieh Sadat <98463228+najmiehsa@users.noreply.github.com>
2022-07-16 22:29:18 +0430

    Update ErrorMessage.test.js

--- FormikComponentsJestTests/ErrorMessage.test.js
+++ FormikComponentsJestTests/ErrorMessage.test.js
@@ -1,1 +1,6 @@
+// myForm.test.js
+import React from 'react'
+import {render, screen, waitFor} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
+import {ErrorMessage} from './ErrorMessage.js'
